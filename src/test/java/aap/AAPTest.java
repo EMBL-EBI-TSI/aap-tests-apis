16e7cd7aa56b57eeac92dea505b5bca6226332ee
Amélie Cornélis <ameliec@ebi.ac.uk>
2018-05-09 16:15:38 +0100

    Generate human friendly reports
    Also, run the tests in parallel. Which means we have to get more
    precision when building our unique timestamps!

--- src/test/java/aap/AAPTest.java
+++ src/test/java/aap/AAPTest.java
@@ -1,11 +1,40 @@
 package aap;
 
-import com.intuit.karate.junit4.Karate;
+import com.intuit.karate.cucumber.CucumberRunner;
+import com.intuit.karate.cucumber.KarateStats;
 import cucumber.api.CucumberOptions;
-import org.junit.runner.RunWith;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
 
-@RunWith(Karate.class)
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+
+// important: do not use @RunWith(Karate.class) ! Because we are using CucumberRunner below
 @CucumberOptions(tags = {"~@Ignore", "~@WIP"})
 public class AAPTest {
+
+    @Test
+    public void testParallel() {
+        String karateOutputPath = "build/test-results/surefire-reports";
+        KarateStats stats = CucumberRunner.parallel(getClass(), 5, karateOutputPath);
+        generateReport(karateOutputPath);
+        Assert.assertThat("There are scenario failures", stats.getFailCount(), is(0));
+    }
+
+    private static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("build/reports"), "aap");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 
 }
--- src/test/java/timestamp.js
+++ src/test/java/timestamp.js
@@ -1,6 +1,6 @@
 function() {
   var SimpleDateFormat = Java.type('java.text.SimpleDateFormat');
-  var sdf = new SimpleDateFormat('yyyyMMdd-MMmmss');
+  var sdf = new SimpleDateFormat('yyyyMMdd-HHmmssSSS');
   var date = new java.util.Date();
   return sdf.format(date);
 }
